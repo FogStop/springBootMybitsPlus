dbe421a7c52bd64a504b1b6ccd3b52aa34f6f4df
123 <123>
2023-09-12 16:04:37 +0800

    第三天内容添加

--- src/main/java/cn/fog/SmpApplication.java
+++ src/main/java/cn/fog/SmpApplication.java
@@ -1,15 +1,18 @@
 package cn.fog;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 @MapperScan("cn.fog.mapper")
 public class SmpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmpApplication.class, args);
+		log.info("项目启动成功");
 	}
 
 }
